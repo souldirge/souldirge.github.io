1e317d1a40b6e061b17c2bc78d0374bab47041ce
souldirge <xyairpwmj@163.com>
2025-04-06 22:35:06 +0800

    Site updated: 2025-04-06 22:35:06

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2025/04/04/Linux 在线练习/","2025/04/04/第一篇文章/","2025/04/04/hello-world/"];function toRandomPost(){
+var posts=["2025/04/04/Linux 在线练习/","2025/04/06/ps命令简介/","2025/04/04/第一篇文章/","2025/04/04/hello-world/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
