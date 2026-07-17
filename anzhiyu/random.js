57077fd70a1f4dc586cd24cdfa1698666a16e1a4
souldirge <xyairpwmj@163.com>
2026-07-17 16:33:00 +0800

    Site updated: 2026-07-17 16:32:57

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2025/04/04/Linux 在线练习/","2025/11/24/test/","2025/11/24/hello-world/","2025/11/24/零成本搭建HEXO博客/","2025/04/06/ps命令简介/","2025/04/04/第一篇文章/"];function toRandomPost(){
+var posts=["2025/04/04/Linux 在线练习/","2026/07/17/纪录片解读-《大国崛起》/","2025/11/24/test/","2025/11/24/hello-world/","2025/11/24/零成本搭建HEXO博客/","2025/04/06/ps命令简介/","2025/04/04/第一篇文章/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
